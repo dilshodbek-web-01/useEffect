3e7ec0ab32ee82e7723b49ea9d92586999b47d8e
dilshodbek-web-01 <dilshodbekkomilov.012@gmail.com>
2022-12-13 20:11:50 +0500

    error

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Main from "./components/Main";
+import Section from "./components/Section";
+import Extra from "./components/Extra";
+import Todos from './components/Todos';
+import Effect from "./components/Effect";
+
+const App = () => {
+  return (
+    <>
+      <Effect />
+
+      {/* <Todos /> */}
+
+      {/* <Main /> */}
+
+      {/* <Section /> */}
+
+      {/* <Extra /> */}
+
+    </>
+  );
+};
+
+export default App;
